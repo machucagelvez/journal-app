c1a25650d8bae72c2b41be6bb846a137453d34ba
Fabián <machucagelvez@gmail.com>
2024-04-03 17:50:41 -0500

    Navbar y sidebar

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,16 @@
 import { MailOutline } from '@mui/icons-material'
 import { Typography } from '@mui/material'
+import { JournalLayout } from '../layout/JournalLayout'
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+        a voluptatem fuga, maiores amet quaerat ratione, ipsum error
+        nisi natus incidunt pariatur iusto velit facere eum ullam,
+        corporis provident nesciunt!
+      </Typography>
+    </JournalLayout>
   )
 }
